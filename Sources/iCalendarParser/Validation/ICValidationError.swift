0d28672dc77f630d28207081a9fc19a511022998
Yuki Y <yymmt3.waste@gmail.com>
2026-07-17 08:29:17 +0200

    feat: validate required properties (#32)

--- Sources/iCalendarParser/Validation/ICValidationError.swift
+++ Sources/iCalendarParser/Validation/ICValidationError.swift
@@ -2,7 +2,9 @@ import Foundation
 
 public enum ICValidationError: Equatable {
     case missingProductIdentifier
+    case missingCalendarVersion
     case missingEventDateStamp(uid: String)
+    case missingEventDateStart(uid: String)
     case missingEventUID
     case mutuallyExclusiveEventDateEndAndDuration(uid: String)
 }
